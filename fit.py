c943743af8cf77a5895b84cfaa3c610edd8f5259
Karim <karim.alaael-din@gmx.de>
2021-02-21 02:54:55 +0000

    WIP: model ALMOST works RIP

--- fit.py
+++ fit.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from filter_norm import filter_norm_split
 from model import CropModel
@@ -9,6 +10,3 @@ crop_model = CropModel()
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
 
-crop_model.compile(optimizer=optimizer, loss="mse", metrics="mae")
-
-crop_model.fit(x_tr, y_tr, epochs=100, verbose=2)
--- model.py
+++ model.py
@@ -13,17 +13,16 @@ class CropModel(tf.keras.Model):
 
     def __init__(self, filters=1, kernel_size=5):
         super(CropModel, self).__init__()
-        self.inp = layers.InputLayer(input_shape=[None, 23, 2], ragged=True)
         self.conv_1 = layers.Conv1D(filters, kernel_size, input_shape=(20, 2))
         self.dense_1 = layers.Dense(20)
         self.dense_2 = layers.Dense(10)
         self.dense_out = layers.Dense(1)
 
     def call(self, inputs):
-        x = self.inp(inputs)
-        x = self.conv_1(x)
+        x = self.conv_1(inputs)
         x = x[:, :, :, 0]
         x = self.dense_1(x)
         x = self.dense_2(x)
         x = self.dense_out(x)
+        print(x)
         return layers.AveragePooling1D(inputs.shape[1])(x)
